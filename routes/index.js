99fdbab1cc0877a4117eb9c05696f062e0090f24
Dmitri Brereton <mitrikyle@yahoo.com>
2016-02-28 03:12:29 -0800

    initial and final? commit

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var accountSid = 'AC74d3f913d8417e3da6fe763e7119db38';
+var authToken = "3efc44c6e1aa92a223d77e6445bcf5ed";
+var client = require('twilio')(accountSid, authToken);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// post to the api, which tells the twilio api to input those digits
+router.get('/callAndInput/:numbers', function(req,res,next){
+  // Download the Node helper library from twilio.com/docs/node/install
+// These vars are your accountSid and authToken from twilio.com/user/account
+  var waitForFiveSeconds = "wwwwwwwwww";
+  var digitString = '';
+  console.log(req.params.numbers);
+  var digits = req.params.numbers.split('');
+  digits.forEach(function(digit){
+    // always wait 5 seconds before hitting the next digitt
+    digitString += (waitForFiveSeconds + digit);
+  });
+  console.log("DAT STRING: " + digitString);
+  client.calls.create({
+    url: "http://twimlbin.com/external/280b4bff4294310842a0149decd2b29b",
+    to: "+14804943646",
+    from: "+12028499415",
+    sendDigits: digitString
+  }, function(err, call) {
+    process.stdout.write(call.sid);
+  });
+});
+
+router.get('/returnCall/:number', function(req,res,next){
+
+});
+
+// get the api to add person to the conference call
+
+module.exports = router;
